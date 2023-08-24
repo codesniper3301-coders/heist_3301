f4f1e539c03ecd4b851ff87a8ac5f0419d87905b
default <default@123.com>
2023-08-24 21:53:08 +0530

    0

--- main/push.py
+++ main/push.py
@@ -2,4 +2,4 @@ from os import system
 system('git add .')
 system(f'git commit -m {system("whoami")}')
 system('git push https://github.com/codesniper3301-coders/heist_3301.git')
-system('Done')
+print('Done')
